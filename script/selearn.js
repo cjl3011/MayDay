eb78b0811db0574023737f9b9b911c86b2859776
dongchao <dongchao@xiaomi.com>
2014-05-24 12:54:03 +0800

    dev

---
+++ script/selearn.js
@@ -0,0 +1,21 @@
+// selearn.js
+// ==========
+
+// > (c) 2014-2015 dongchao chenjinglun
+// > barrage may be freely distributed under the MIT license.
+// > For all details and documentation:
+
+// Initial Setup
+// -------------
+
+
+(function() {
+
+  // Reload info
+  console.log("the editor reload the selearn Module!!!");
+  var root = this;
+  var Selearn;
+  Selearn.VERSION = '0.0.1';
+  
+  
+}).call(this);
