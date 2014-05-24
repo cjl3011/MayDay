133c3d597dd1e3dd308fdca566954611a94cf93e
atupal <atupalykl@gmail.com>
2014-05-24 11:22:53 +0000

    ace.js api change: initValue -> setValue

--- script/menu.js
+++ script/menu.js
@@ -32,7 +32,8 @@ filemenu.submenu.append(new gui.MenuItem({
 	label: '新建 (&N)',
 	click: function() {
 		editor.window.setTitleEmpty();
-		editor.initValue('');
+		//editor.initValue('');
+		editor.setValue('');
 		$('#fileDialogSave').attr("file", '');
 	}
 }));
